007ce48bf533305ef211b11473b5fd8a5789bc10
Sebastian <sebastian>
2016-12-11 23:15:44 -0300

    Se quitó el error  entre de archivos binarios.

--- Moraga/PROYECTOFINAL.py
+++ Moraga/PROYECTOFINAL.py
@@ -3,7 +3,8 @@ import shapefile
 import numpy
 import matplotlib.pyplot as plt
 import ogr, osr # Paquetes que vienen con GDAL
-from scipy import integrate, cos, sin, pi, arctan, sqrt
+from scipy import integrate, cos, sin, pi, arctan, arctan2, sqrt
+from tqdm import tqdm # Barra de progreso para ciclos largos
 from H import H # Esto está en H.py
 
 # Este código debe ejecutarse en el mismo directorio donde están
@@ -22,6 +23,31 @@ def separarPartes(puntos, inicioPartes):
             puntos_parte_k = puntos[inicioPartes[k]:, :]
         salida.append(puntos_parte_k)
     return salida
+
+# Funciones que transforman entre longitud-latitud (grados) y
+# ángulo azimutal-ángulo polar/colatitud (radianes)
+def lonlat2phitheta(lon, lat):
+    phi = lon*pi/180.0
+    theta = pi/2.0 - lat*pi/180.0
+    return phi, theta
+def phitheta2lonlat(phi, theta):
+    lon = phi*180.0/pi
+    lat = 90.0 - theta*180.0/pi
+    return lon, lat
+# Funciones que transforman entre coordenadas cartesianas y coordenadas
+# esféricas (en el orden radio, ángulo azimutal, ángulo polar/colatitud)
+def cart2sph(x, y, z):
+    r = sqrt(x**2 + y**2 + z**2)
+    theta = arctan2(sqrt(x**2+y**2), z)
+    phi = arctan2(y, x)
+    return r, theta, phi
+def sph2cart(r, theta, phi):
+    x = r*cos(phi)*sin(theta)
+    y = r*sin(phi)*sin(theta)
+    z = r*cos(theta)
+    return x, y, z
+
+
 #Funciones integrales
 def Ix(Pi,Pi1,Ti,Ti1):
     F= lambda s: -(sin(Ti - s*(Ti - Ti1)))*(sin(Ti - s*(Ti - Ti1)))*sin(Pi - s*(Pi - Pi1))*(Ti - Ti1)
@@ -49,11 +75,11 @@ def Areap(Pi,Pi1,Ti,Ti1):
 # Código principal
 sf = shapefile.Reader("division_comunal")
 ncomunas = 346
-R=6371000
-IX=0
-IY=0
-IZ=0
-POBLA=0
+R = 6371000.0
+IX = 0.0
+IY = 0.0
+IZ = 0.0
+POBLA = 0.0
 # Cómputos geográficos preliminares
 inputEPSG = 32719
 outputEPSG = 4326
@@ -63,69 +89,60 @@ outSpatialRef = osr.SpatialReference()
 outSpatialRef.ImportFromEPSG(outputEPSG)
 coordTransform = osr.CoordinateTransformation(inSpatialRef, outSpatialRef)
 # Alocación de memoria para integrales sobre comunas
-INTpspx = numpy.zeros(ncomunas)
-INTpspy = numpy.zeros(ncomunas)
-INTpspz = numpy.zeros(ncomunas)
-AREApsp = numpy.zeros(ncomunas)
-Densicomunal= numpy.zeros(ncomunas)
+intComX = numpy.zeros(ncomunas)
+intComY = numpy.zeros(ncomunas)
+intComZ = numpy.zeros(ncomunas)
+intCom1 = numpy.zeros(ncomunas)
+# Variable para contener las geometrías de las comunas
+psp = [None]*ncomunas
 # Ciclo por comunas
-for j, comuna in enumerate(sf.shapeRecords()):
+for j, comuna in enumerate(tqdm(sf.shapeRecords())):
     nombre = comuna.record[2]
     cods = comuna.record[6]
     inicioPartes = comuna.shape.parts
     puntos = numpy.array(comuna.shape.points)
-    psp = separarPartes(puntos, inicioPartes)
-    for k in range(0,len(psp)):
-        for i in range(0,len(psp[k])):
-             pointX = psp[k][i,0]
-             pointY = psp[k][i,1]
+    psp[j] = separarPartes(puntos, inicioPartes)
+    # Ciclo por polígonos
+    for k in range(0,len(psp[j])):
+        # Ciclo por vértices
+        for i in range(0,len(psp[j][k])):
+             pointX = psp[j][k][i,0]
+             pointY = psp[j][k][i,1]
              point = ogr.Geometry(ogr.wkbPoint)
              # create a geometry from coordinates
              point.AddPoint(pointX, pointY)
              # transform point
              point.Transform(coordTransform)
-             # print point in EPSG 4326
-#PRUEBA
-             RADy=(point.GetX())*pi/180        #ANGULO LONGITUD (PHI)
-             RADx=-((point.GetY()*pi/180)+pi/2)#ANGULO CO-LATITUD (THETA)
-             psp[k][i,0]=RADy # PHI
-             psp[k][i,1]=RADx # THETA
-	plt.plot(psp[k][:,0],psp[k][:,1])	
-        #zona poligonal en coordenadas lat/long
-        AREAi=0
-        INTxi=0
-        INTyi=0
-        INTzi=0
-        for i in range(0,len(psp[k])-1):
-            Pi=psp[k][i,0]     # PHI
-            Pi1=psp[k][i+1,0]  
-            Ti=psp[k][i,1]     # THETA
-            Ti1=psp[k][i+1,1]
-            IIX=Ix(Pi,Pi1,Ti,Ti1)
-            IIY=Iy(Pi,Pi1,Ti,Ti1)
-            IIZ=Iz(Pi,Pi1,Ti,Ti1)
-            A=Areap(Pi,Pi1,Ti,Ti1)
-            AREAi=AREAi+A
-            INTxi=INTxi+IIX
-            INTyi=INTyi+IIY
-            INTzi=INTzi+IIZ
-        INTpspx[j] += INTxi
-        INTpspy[j] += INTyi
-        INTpspz[j] += INTzi
-        AREApsp[j] += AREAi
-    Densicomunal[j]+=H[j]/AREApsp[j]
-    ICX=Densicomunal[j]*R*INTpspx[j]
-    ICy=Densicomunal[j]*R*INTpspy[j]
-    ICz=Densicomunal[j]*R*INTpspz[j]
-    IX=IX+ICX
-    IY=IY+ICy
-    IZ=IZ+ICz
-    POBLA=POBLA+H[j]
+             # Sobreescribimos con nuevas coordenadas: De longitud-latitud a
+             # ángulo azimutal (φ)-ángulo polar/colatitud (θ)
+             psp[j][k][i,0], psp[j][k][i,1] = \
+                     lonlat2phitheta(point.GetX(), point.GetY())
+        AREAk = 0.0
+        INTxk = 0.0
+        INTyk = 0.0
+        INTzk = 0.0
+        # Ciclo por segmentos
+        for i in range(0,len(psp[j][k])-1):
+            #                  φ[i]            φ[i+1]             θ[i]           θ[i+1]
+            #                    ↓               ↓                 ↓               ↓ 
+            AREAk += Areap(psp[j][k][i,0], psp[j][k][i+1,0], psp[j][k][i,1], psp[j][k][i+1,1])
+            INTxk += Ix(psp[j][k][i,0], psp[j][k][i+1,0], psp[j][k][i,1], psp[j][k][i+1,1])
+            INTyk += Iy(psp[j][k][i,0], psp[j][k][i+1,0], psp[j][k][i,1], psp[j][k][i+1,1])
+            INTzk += Iz(psp[j][k][i,0], psp[j][k][i+1,0], psp[j][k][i,1], psp[j][k][i+1,1])
+        intComX[j] += INTxk
+        intComY[j] += INTyk
+        intComZ[j] += INTzk
+        intCom1[j] += AREAk
+    Densicomunal=H[j]/intCom1[j]
+    IX += Densicomunal*R*intComX[j]
+    IY += Densicomunal*R*intComY[j]
+    IZ += Densicomunal*R*intComZ[j]
+    POBLA += H[j]
 #COORDENADAS EN X Y Z
-X=IX/POBLA
-Y=IY/POBLA
-Z=IZ/POBLA
-THETA=(arctan(sqrt(X*X+Y*Y)/Z))*180/pi-90    #ANGULO EN Latitud
-PHI=((arctan(Y/X))*180/pi)        #ANGULO EN Longitud
-print("LON",PHI)
-print("LAT",THETA)
+X = IX/POBLA
+Y = IY/POBLA
+Z = IZ/POBLA
+R, THETA, PHI = cart2sph(X, Y, Z)
+LON, LAT = phitheta2lonlat(PHI, THETA)
+print("LON", LON)
+print("LAT", LAT)
